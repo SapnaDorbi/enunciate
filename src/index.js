06830763aae210bacf614d372281f39fc63ba396
SapnaDorbi <savydorbi@gmail.com>
2021-01-17 00:26:05 +0530

    implemented validation in register form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// import reducers from './reducers';
+// import { createStore, applyMiddleware, compose } from 'redux';
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(
+//   reducers,
+//   composeEnhancers(applyMiddleware)
+// );
 
 const Root = () => (
   <Router>
@@ -19,8 +27,3 @@ const Root = () => (
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
