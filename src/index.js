c61c045db5622604a7de8133c37f91c3c4617454
SapnaDorbi <savydorbi@gmail.com>
2021-01-23 01:31:56 +0530

    created actions and reducer for setUser

--- src/index.js
+++ src/index.js
@@ -7,20 +7,19 @@ import Register from './components/Auth/Register';
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import  firebase from './firebase';
+import { compose, createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
 
-// import reducers from './reducers';
-// import { createStore, applyMiddleware, compose } from 'redux';
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(
-//   reducers,
-//   composeEnhancers(applyMiddleware)
-// );
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if(user) {
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     });
@@ -36,11 +35,13 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById('root')
  );
